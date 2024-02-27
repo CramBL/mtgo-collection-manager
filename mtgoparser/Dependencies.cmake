e6888b5f06252fdff477f34c10934d470734f988
Marc Beck König <mbkj@tutamail.com>
2024-02-27 16:45:12 +0100

    bump glaze to 2.1.8

--- mtgoparser/Dependencies.cmake
+++ mtgoparser/Dependencies.cmake
@@ -9,7 +9,7 @@ function(mtgoparser_setup_dependencies)
   # already been provided to us by a parent project
 
   if(NOT TARGET glaze::glaze)
-    cpmaddpackage("gh:stephenberry/glaze@2.1.4")
+    cpmaddpackage("gh:stephenberry/glaze@2.1.8")
   endif()
 
   if(NOT TARGET fmtlib::fmtlib)
