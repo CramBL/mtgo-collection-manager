2894fb4c38f90593d14c6c4a34981c9a145a1cf1
Marc Konig <mbkj@tutamail.com>
2024-04-30 20:49:08 +0200

    initial rust-based mtgogetter

---
+++ mtgogetter-rs/src/lib.rs
@@ -0,0 +1,27 @@
+use reqwest::Url;
+
+const GOATBOTS_PRICE_HISTORY_URL: &str = "https://www.goatbots.com/download/price-history.zip";
+
+pub fn get_goatbots_price_history() {
+    let bytes = reqwest::blocking::get(GOATBOTS_PRICE_HISTORY_URL)
+        .unwrap()
+        .bytes()
+        .unwrap();
+
+    eprintln!("Got {} bytes", bytes.len());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_goatbots_price_history_url() {
+        assert!(Url::parse(GOATBOTS_PRICE_HISTORY_URL).is_ok());
+    }
+
+    #[test]
+    fn test_get_goatbots_price_history() {
+        get_goatbots_price_history();
+    }
+}
