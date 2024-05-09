b8558f4bf42264c448daa8b00d9cf711465bad24
Marc Beck König <mbkj@tutamail.com>
2024-05-09 11:46:10 +0200

    implement getting and extracting prices

--- mtgogetter-rs/src/lib.rs
+++ mtgogetter-rs/src/lib.rs
@@ -1,17 +1,24 @@
 use reqwest::Url;
+use std::io::{Cursor, Read};
 
 pub mod state_log;
 
-
 const GOATBOTS_PRICE_HISTORY_URL: &str = "https://www.goatbots.com/download/price-history.zip";
 
-pub fn get_goatbots_price_history() {
+pub fn get_goatbots_price_history() -> String {
     let bytes = reqwest::blocking::get(GOATBOTS_PRICE_HISTORY_URL)
         .unwrap()
         .bytes()
         .unwrap();
 
     eprintln!("Got {} bytes", bytes.len());
+    let cursor = Cursor::new(bytes);
+    let mut archive = zip::ZipArchive::new(cursor).unwrap();
+
+    let mut file = archive.by_index(0).unwrap();
+    let mut contents = String::with_capacity(512 * 1024);
+    file.read_to_string(&mut contents).unwrap();
+    contents
 }
 
 #[cfg(test)]
@@ -25,6 +32,7 @@ mod tests {
 
     #[test]
     fn test_get_goatbots_price_history() {
-        get_goatbots_price_history();
+        let res = get_goatbots_price_history();
+        eprintln!("{res}");
     }
 }
